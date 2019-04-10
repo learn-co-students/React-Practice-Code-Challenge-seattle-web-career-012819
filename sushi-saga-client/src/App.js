72b68ef12c302b32373d5c970056142bacf2dee1
ScottSlatton <scottslatton.tech@gmail.com>
2019-04-10 10:01:00 -0700

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,17 +1,75 @@
-import React, { Component } from 'react';
-import SushiContainer from './containers/SushiContainer';
-import Table from './containers/Table';
+import React, { Component } from "react";
+import SushiContainer from "./containers/SushiContainer";
+import Table from "./containers/Table";
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      sushies: [],
+      index: 0,
+      balance: 100,
+      eaten: []
+    };
+    this.getSushies();
+    this.getFour = this.getFour.bind(this);
+    this.eatSushi = this.eatSushi.bind(this);
+  }
+
+  getSushies() {
+    fetch(API)
+      .then(res => res.json())
+      .then(sushies => {
+        this.setSushies(sushies);
+      });
+  }
+
+  setSushies(sushies) {
+    let eatSushies = sushies.map(sushi => {
+      sushi.isEaten = false;
+      return sushi;
+    });
+    this.setState({ sushies: eatSushies });
+  }
+
+  getFour() {
+    if (this.state.index >= 96) {
+      this.setState({
+        index: 0
+      });
+    } else {
+      let newIndex = this.state.index + 4;
+      this.setState({
+        index: newIndex
+      });
+    }
+  }
+  eatSushi(sushi) {
+    if (this.state.balance > sushi.price && !sushi.isEaten) {
+      sushi.isEaten = true;
+      this.state.eaten.push(sushi);
+      this.setState({
+        eaten: this.state.eaten,
+        balance: this.state.balance - sushi.price
+      });
+    } else {
+      return;
+    }
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+          sushies={this.state.sushies}
+          index={this.state.index}
+          getFour={this.getFour}
+          eatSushi={this.eatSushi}
+        />
+        <Table balance={this.state.balance} eaten={this.state.eaten} />
       </div>
     );
   }
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,23 +1,20 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
-const Sushi = (props) => {
+const Sushi = props => {
   return (
-    <div className="sushi">
-      <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          true ?
-            null
-          :
-            <img src={/* Give me an image source! */} width="100%" />
-        }
+    <Fragment>
+      <div className="sushi">
+        <div className="plate" onClick={() => props.eatSushi(props.sushi)}>
+          {props.sushi.isEaten ? null : (
+            <img src={props.sushi.img_url} alt="Yummy Sushi" width="100%" />
+          )}
+        </div>
+        <h4 className="sushi-details">
+          {props.sushi.name} - ${props.sushi.price}
+        </h4>
       </div>
-      <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
-      </h4>
-    </div>
-  )
-}
+    </Fragment>
+  );
+};
 
-export default Sushi
+export default Sushi;
